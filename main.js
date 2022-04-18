9501c05fa87fea9fb9a796c98698605c5feb55b2
Patrick Dang <pdang011@ucr.edu>
2022-04-18 15:58:44 -0700

    Setting up the lab

---
+++ main.js
@@ -0,0 +1,6 @@
+const url = "http://ec2-18-209-247-77.compute-1.amazonaws.com:3000/feed/random?q=weather";
+
+// Add all event listeners after document has loaded in
+document.addEventListener('DOMContentLoaded', () => {
+
+});
